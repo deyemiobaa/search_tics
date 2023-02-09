ef8e3d000898207e6a8ec314a85fad6af9421a68
sodiq <sodiqaderibigbe90@gmail.com>
2023-02-09 10:30:13 +0100

    add devise with custom views

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'user object' do
+    it 'is valid with valid attributes' do
+      user = User.create(email: 'test@mailer.com', password: 'password', password_confirmation: 'password')
+      expect(user).to be_valid
+    end
+
+    it 'is not valid without a email' do
+      user = User.create(email: nil, password: 'password', password_confirmation: 'password')
+      expect(user).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      user = User.create(email: 'test@mailer.com', password: nil, password_confirmation: nil)
+      expect(user).to_not be_valid
+    end
+
+    it 'defaults to not being an admin' do
+      user = User.create(email: 'test@mailer.com', password: 'password', password_confirmation: 'password')
+      expect(user.admin).to eq(false)
+    end
+  end
+end
