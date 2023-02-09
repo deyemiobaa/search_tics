dcfdef9019fb17872f7c7982aa8db0d97af59ad7
sodiq <sodiqaderibigbe90@gmail.com>
2023-02-09 11:07:25 +0100

    generate search page

---
+++ spec/requests/homepage_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Homepages", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      get root_path
+      expect(response).to render_template("index")
+    end
+  end
+
+end
